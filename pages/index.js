3c6e9124a3d949f67915c9d4325755492a2eb120
Emerson Pombo <emersonpombo5@gmail.com>
2025-07-17 08:33:42 -0300

    chore: :zap: Configuração do Prettier

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 function Home() {
-    return <h1>
-        "Dedico esta minha primeira aplicação completa, desenvolvida de ponta a ponta, ao melhor Tech Lead que já tive, @Jannsen Brauer. 
-        Ele sempre me incentivou a buscar o melhor de mim, me motivou nos momentos em que mais precisei, me orientou com paciência, 
-        estendeu a mão quando tive dúvidas e me ajudou a enxergar um caminho — e, acima de tudo, a acreditar no meu potencial."
+  return (
+    <h1>
+      "Dedico esta minha primeira aplicação completa, desenvolvida de ponta a
+      ponta, ao melhor Tech Lead que já tive, @Jannsen Brauer. Ele sempre me
+      incentivou a buscar o melhor de mim, me motivou nos momentos em que mais
+      precisei, me orientou com paciência, estendeu a mão quando tive dúvidas e
+      me ajudou a enxergar um caminho — e, acima de tudo, a acreditar no meu
+      potencial."
     </h1>
+  );
 }
 
 export default Home;
