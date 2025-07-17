8f066f18e8c17f5dd068787522b6cda9dd60ddb1
Emerson Pombo <emersonpombo5@gmail.com>
2025-07-17 04:37:40 -0300

    initial

---
+++ .next/server/pages/index.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
+exports.modules = {
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction Home() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n        children: '\"Dedico esta minha primeira aplica\\xe7\\xe3o completa, desenvolvida de ponta a ponta, ao melhor Tech Lead que j\\xe1 tive, @Jannsen Brauer. Ele sempre me incentivou a buscar o melhor de mim, me motivou nos momentos em que mais precisei, me orientou com paci\\xeancia, estendeu a m\\xe3o quando tive d\\xfavidas e me ajudou a enxergar um caminho — e, acima de tudo, a acreditar no meu potencial.\"'\n    }, void 0, false, {\n        fileName: \"/home/emerson-pombo/Documentos/projetos/clone-tabnews/pages/index.js\",\n        lineNumber: 2,\n        columnNumber: 12\n    }, this);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Home);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQUEsU0FBU0EsT0FBTztJQUNaLHFCQUFPLDhEQUFDQztrQkFBRzs7Ozs7O0FBS2Y7QUFFQSxpRUFBZUQsSUFBSUEsRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL2Nsb25lLXRhYm5ld3MvLi9wYWdlcy9pbmRleC5qcz9iZWU3Il0sInNvdXJjZXNDb250ZW50IjpbImZ1bmN0aW9uIEhvbWUoKSB7XG4gICAgcmV0dXJuIDxoMT5cbiAgICAgICAgXCJEZWRpY28gZXN0YSBtaW5oYSBwcmltZWlyYSBhcGxpY2HDp8OjbyBjb21wbGV0YSwgZGVzZW52b2x2aWRhIGRlIHBvbnRhIGEgcG9udGEsIGFvIG1lbGhvciBUZWNoIExlYWQgcXVlIGrDoSB0aXZlLCBASmFubnNlbiBCcmF1ZXIuIFxuICAgICAgICBFbGUgc2VtcHJlIG1lIGluY2VudGl2b3UgYSBidXNjYXIgbyBtZWxob3IgZGUgbWltLCBtZSBtb3Rpdm91IG5vcyBtb21lbnRvcyBlbSBxdWUgbWFpcyBwcmVjaXNlaSwgbWUgb3JpZW50b3UgY29tIHBhY2nDqm5jaWEsIFxuICAgICAgICBlc3RlbmRldSBhIG3Do28gcXVhbmRvIHRpdmUgZMO6dmlkYXMgZSBtZSBhanVkb3UgYSBlbnhlcmdhciB1bSBjYW1pbmhvIOKAlCBlLCBhY2ltYSBkZSB0dWRvLCBhIGFjcmVkaXRhciBubyBtZXUgcG90ZW5jaWFsLlwiXG4gICAgPC9oMT5cbn1cblxuZXhwb3J0IGRlZmF1bHQgSG9tZTsiXSwibmFtZXMiOlsiSG9tZSIsImgxIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/index.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/index.js"));
+module.exports = __webpack_exports__;
+
+})();
